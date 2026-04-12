62af995e02176170eee9dd4d8de11ae18637c54b
JAVIERA PINTO <jpints@uc.cl>
2026-04-11 21:12:13 -0400

    feat: add function to connect interface and js

--- functions.js
+++ functions.js
@@ -1,102 +1,131 @@
 // Referecia de función: https://medium.com/@randomstr/generating-random-strings-in-javascript-and-its-frameworks-118cb1c9cba7
 function generateRandomString(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-
 const validarPasaporte = (id) => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            if (id % 2 == 0) {
-                reject("El id es inválido");
-            } else {
-                resolve("El id es válido")
-            }
-        }, 1500);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (id % 2 == 0) {
+        reject("El id es inválido");
+      } else {
+        resolve("El id es válido");
+      }
+    }, 1500);
+  });
 };
 
 // Referencias de Math.random(): https://www.w3schools.com/Js/js_random.asp
 const verificarRestriccionesVisa = (id) => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            const prob = Math.random();
-            if (prob <= 0.3) {
-                reject("Visa no válida para el destino");
-            } else {
-                resolve("Visa válida para el destino");
-            }
-        }, 2000);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const prob = Math.random();
+      if (prob <= 0.3) {
+        reject("Visa no válida para el destino");
+      } else {
+        resolve("Visa válida para el destino");
+      }
+    }, 2000);
+  });
 };
 
 const asignarAsiento = () => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            const randomString = generateRandomString(3);
-            resolve(randomString);
-        }, 1000);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const randomString = generateRandomString(3);
+      resolve(randomString);
+    }, 1000);
+  });
 };
 
 const generarPaseAbordar = (pasaporte, visa, asiento) => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve({pasaporte, visa, asiento});
-        }, 500);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve({ pasaporte, visa, asiento });
+    }, 500);
+  });
 };
 
-
-
-
 const conTimeout = (promesa, ms) =>
-    Promise.race([
-        promesa,
-        new Promise((_, reject) =>
-            setTimeout(() => reject("Tiempo de espera agotado"), ms)
-        )
-    ]);
+  Promise.race([
+    promesa,
+    new Promise((_, reject) =>
+      setTimeout(() => reject("Tiempo de espera agotado"), ms),
+    ),
+  ]);
 
 const iniciarCheckIn = (pasajeroId) => {
+  const proceso = Promise.all([
+    // Verificar pasaporte y visa en paralelo para optimizar el tiempo
+    validarPasaporte(pasajeroId).then((resultado) => {
+      return resultado;
+    }),
+    verificarRestriccionesVisa(pasajeroId).then((resultado) => {
+      return resultado;
+    }),
+  ])
 
-    const proceso = Promise.all([
-        // Verificar pasaporte y visa en paralelo para optimizar el tiempo
-        validarPasaporte(pasajeroId).then((resultado) => {
-            return resultado;
-        }),
-        verificarRestriccionesVisa(pasajeroId).then((resultado) => {
-            return resultado;
-        })
-    ])
+    // Si ambos procesos son exitosos, asignar asiento y generar pase de abordar
+    .then(([pasaporte, visa]) => {
+      return asignarAsiento().then((asiento) => ({
+        pasajeroId,
+        pasaporte,
+        visa,
+        asiento,
+      }));
+    })
 
-        // Si ambos procesos son exitosos, asignar asiento y generar pase de abordar
-        .then(([pasaporte, visa]) => {
-            return asignarAsiento()
-                .then((asiento) => ({ pasajeroId, pasaporte, visa, asiento }));
-        })
+    // Generar pase de abordar con los datos obtenidos
+    .then((datos) => {
+      return generarPaseAbordar(datos.pasaporte, datos.visa, datos.asiento);
+    })
 
-        // Generar pase de abordar con los datos obtenidos
-        .then((datos) => {
-            return generarPaseAbordar(datos.pasaporte, datos.visa, datos.asiento);
-        })
+    // Se genera el pase de abordar
+    .then((pase) => {
+      return pase;
+    })
 
-        // Se genera el pase de abordar
-        .then((pase) => {
-            return pase;
-        })
+    // Manejo de errores en cualquier etapa del proceso
+    .catch((error) => {
+      console.error(`Error: ${error}`);
+      throw error;
+    });
 
-        // Manejo de errores en cualquier etapa del proceso
-        .catch((error) => {
-            console.error(`Error: ${error}`);
-            throw error;
-        });
+  return conTimeout(proceso, 4000);
+};
 
-    return conTimeout(proceso, 4000);
+const validarID = () => {
+  const id = document.getElementById("input-id").value;
+  const boton = document.getElementById("button-id");
+  const logs = document.getElementById("logs-id");
+  const passport = document.getElementById("passport-info");
+  passport.style.display = "none";
+  logs.innerHTML = ``;
+  passport.innerHTML = `<h1>✈️ Información del vuelo</h1>`;
+  boton.disable = true;
+  logs.style.marginTop = "2rem";
+  logs.innerHTML += `<p>Iniciando validaciones</p>`;
+  boton.innerText = "Cargando...";
+
+  iniciarCheckIn(id)
+    .then((resultado) => {
+      logs.innerHTML = `<p>¡Pasaporte validado!</p>`;
+      passport.innerHTML += `<p>Asiento ${resultado.asiento}</p>`;
+      passport.style.display = "grid";
+    })
+    .catch((error) => {
+      logs.innerHTML += `<p>${error}</p>`;
+      boton.disable = false;
+      boton.innerText = "Validar";
+    })
+    .finally(() => {
+      boton.disable = false;
+      boton.innerText = "Validar";
+    });
 };
